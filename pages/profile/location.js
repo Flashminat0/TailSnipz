699bbd677fb7f13630c7ec3e667dee9b251a1663
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2022-05-03 17:00:56 +0530

    code arrangement

--- pages/profile/location.js
+++ pages/profile/location.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Link from "next/link";
-import { useState } from 'react'
-import { Switch } from '@headlessui/react'
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
 
 const Location = () => {
-
-    const [enabled, setEnabled] = useState(false)
+  const [enabled, setEnabled] = useState(false);
 
   return (
     <div className={"h-screen w-screen grid place-content-center font susty"}>
@@ -17,11 +16,6 @@ const Location = () => {
         </div>
 
         <div className="mt-10 sm:mt-0">
-
-
-
-
-
           <div className="md:grid md:grid-cols-3 md:gap-6">
             <div className="md:col-span-1">
               <div className="px-4 sm:px-0">
@@ -54,20 +48,11 @@ const Location = () => {
             </div>
           </div>
 
-
-
-
-
-
-
-
-
-
           <div className="md:grid md:grid-cols-3 md:gap-6">
             <div className="md:col-span-1">
               <div className="px-4 sm:px-0">
                 <h3 className="text-lg font-medium leading-6 text-gray-900">
-                    Town/City
+                  Town/City
                 </h3>
               </div>
             </div>
@@ -95,20 +80,11 @@ const Location = () => {
             </div>
           </div>
 
-
-
-
-
-
-
-
-
-
           <div className="md:grid md:grid-cols-3 md:gap-6">
             <div className="md:col-span-1">
               <div className="px-4 sm:px-0">
                 <h3 className="text-lg font-medium leading-6 text-gray-900">
-                Show city in profile
+                  Show city in profile
                 </h3>
               </div>
             </div>
@@ -117,27 +93,20 @@ const Location = () => {
                 <div className="px-4 py-5 bg-white sm:p-6">
                   <div className="grid grid-cols-6 gap-6">
                     <div className="col-span-6 sm:col-span-3">
-
-
-
-                    
-      <Switch
-        checked={enabled}
-        onChange={setEnabled}
-        className={`${enabled ? 'bg-susty' : 'bg-gray-500'}
+                      <Switch
+                        checked={enabled}
+                        onChange={setEnabled}
+                        className={`${enabled ? "bg-susty" : "bg-gray-500"}
           relative inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-      >
-        <span
-          aria-hidden="true"
-          className={`${enabled ? 'translate-x-5' : 'translate-x-0'}
+                      >
+                        <span
+                          aria-hidden="true"
+                          className={`${
+                            enabled ? "translate-x-5" : "translate-x-0"
+                          }
             pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-        />
-      </Switch>
-   
-
-
-
-
+                        />
+                      </Switch>
                     </div>
 
                     <div className="col-span-6 sm:col-span-3"></div>
@@ -146,15 +115,6 @@ const Location = () => {
               </div>
             </div>
           </div>
-
-
-
-
-
-
-
-
-
         </div>
 
         <div className="hidden sm:block" aria-hidden="true">
