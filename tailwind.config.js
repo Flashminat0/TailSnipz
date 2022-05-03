f1cfc85b0e7cf0886c842332ed6f4d95237272c3
Flashminat0 <wyasaslowe@gmail.com>
2022-05-03 13:07:45 +0530

    Required Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
-    fontFamily: {
-        heading: ["Poppins", "sans-serif"],
-        body: ["Poppins", "sans-serif"],
-    },
+
     theme: {
         extend: {
             colors: {
