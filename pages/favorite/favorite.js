b8cc4a2648da20fd69a2be6d46f8e443ed272bd4
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2022-05-09 02:46:09 +0530

    new component added.

--- pages/favorite/favorite.js
+++ pages/favorite/favorite.js
@@ -28,56 +28,36 @@ const NotificationView = () => {
             />
           </a>
 
-        <ul class="mb-2 mt-2 ml-2 w-auto text-sm font-medium text-gray-900 bg-white   bg-white dark:border-gray-600 text-gray-700">
-        <table class="table-fixed">
-  <thead>
-    <tr>
-      <th>Shirt</th>
-    <th>    
-
-        <div class="ml-64">   
-        <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 ml-3 mr-2 hover:text-red-400 focus:text-red-400 cursor-pointer"
-                  fill="none "
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
-                </div> 
-
-
-    </th>
-      <th>
-          <div class="mr-1">5</div>
-    </th>
-    </tr>
-  </thead>
-</table>
-        </ul>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          <ul class="mb-2 mt-2 ml-2 w-auto text-sm font-medium text-gray-900 bg-white   bg-white dark:border-gray-600 text-gray-700">
+            <table class="table-fixed">
+              <thead>
+                <tr>
+                  <th>Shirt</th>
+                  <th>
+                    <div class="ml-64">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 ml-3 mr-2 hover:text-red-400 focus:text-red-400 cursor-pointer"
+                        fill="none "
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                        />
+                      </svg>
+                    </div>
+                  </th>
+                  <th>
+                    <div class="mr-1">5</div>
+                  </th>
+                </tr>
+              </thead>
+            </table>
+          </ul>
         </div>
       </>
     </div>
@@ -85,8 +65,6 @@ const NotificationView = () => {
 };
 
 export default NotificationView;
-
-
 
 /*
 
