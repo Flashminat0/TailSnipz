22a30a144ad60cbe4e2eb19adb620afa1aa7c835
swee76 <oshinikithma1213@gmail.com>
2022-05-10 11:35:12 +0530

    suggested searches ui startup

--- pages/home/suggestedsearches.js
+++ pages/home/suggestedsearches.js
@@ -1,48 +1,100 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ChevronRightIcon} from "@heroicons/react/solid";
 
 const searches = [
-    {id: 1, name: "Victoria's Secret", views: "17.5K views"},
-    {id: 2, name: "Michael Kors", views: "10.7K views"},
-    {id: 3, name: "Vans", views: "6.78K views"},
-    {id: 4, name: "adidas", views: "10.9K views"},
-    {id: 5, name: "Shein", views: "32.1K views"},
-    {id: 6, name: "Ray-Ban", views: "1.45K views"},
-    {id: 7, name: "Converse Shoes", views: "3.64K views"},
-    {id: 8, name: "Levi's Cloths", views: "6.52K views"},
-    {id: 9, name: "Nike", views: "33.8K views"},
-    {id: 10, name: "Zara", views: "19.4K views"},
-    {id: 11, name: "Gymshark", views: "3.22K views"},
-    {id: 12, name: "Brandy Melville", views: "17.5K views"},
-    {id: 13, name: "Ralph Lauren", views: "17.5K views"},
-    {id: 14, name: "Under Armour", views: "17.5K views"},
-    {id: 15, name: "Urban Outfitters", views: "21.5K views"},
-    {id: 16, name: "American Apparel", views: "11.3K views"},
-    {id: 17, name: "Vintage", views: "65.2K views"},
-    {id: 18, name: "The North Face", views: "7.5K views"},
-    {id: 19, name: "Laura Ashley", views: "12.5K views"},
-    {id: 20, name: "Louis Vuitton", views: "3.46K views"},
+    {id: 1, pos: 1, name: "Victoria's Secret", views: "17.5K views", active: true},
+    {id: 2, pos: 2, name: "Michael Kors", views: "10.7K views", active: true},
+    {id: 3, pos: 3, name: "Vans", views: "6.78K views", active: true},
+    {id: 4, pos: 4, name: "adidas", views: "10.9K views", active: true},
+    {id: 5, pos: 5, name: "Shein", views: "32.1K views", active: true},
+    {id: 6, pos: 6, name: "Ray-Ban", views: "1.45K views", active: true},
+    {id: 7, pos: 7, name: "Converse Shoes", views: "3.64K views", active: true},
+    {id: 8, pos: 8, name: "Levi's Cloths", views: "6.52K views", active: true},
+    {id: 9, pos: 9, name: "Nike", views: "33.8K views", active: false},
+    {id: 10, pos: 10, name: "Zara", views: "19.4K views", active: false},
+    {id: 11, pos: 11, name: "Gymshark", views: "3.22K views", active: false},
+    {id: 12, pos: 12, name: "Brandy Melville", views: "17.5K views", active: false},
+    {id: 13, pos: 13, name: "Ralph Lauren", views: "17.5K views", active: false},
+    {id: 14, pos: 14, name: "Under Armour", views: "17.5K views", active: false},
+    {id: 15, pos: 15, name: "Urban Outfitters", views: "21.5K views", active: false},
+    {id: 16, pos: 16, name: "American Apparel", views: "11.3K views", active: false},
+    {id: 17, pos: 17, name: "Vintage", views: "65.2K views", active: false},
+    {id: 18, pos: 18, name: "The North Face", views: "7.5K views", active: false},
+    {id: 19, pos: 19, name: "Laura Ashley", views: "12.5K views", active: false},
+    {id: 20, pos: 20, name: "Louis Vuitton", views: "3.46K views", active: false},
 ];
 
 const Suggestedsearches = () => {
+    const [cards, setCards] = useState(searches);
+
+    const handleLeftClick = (isLeft) => {
+        const prevState = [...cards];
+        // find next inactive card index - top
+        const nextCardIdx = prevState
+            .filter((ft) => ft.active === true)
+            .sort((a, b) => (a.pos > b.pos ? 1 : b.pos > a.pos ? -1 : 0))[0].id;
+        // reset
+        prevState.find((f) => f.active === false).active = true;
+        // update
+        prevState.find((f) => f.id === nextCardIdx).active = false;
+        // maximize pos
+        prevState.find((f) => f.id === nextCardIdx).pos =
+            Math.max.apply(
+                null,
+                prevState.map(function (o) {
+                    return o.pos;
+                })
+            ) + 1;
+
+        // update state
+        setCards(prevState);
+    };
+
+    const handleRightClick = () => {
+        const prevState = [...cards];
+        // find next inactive card index - bottom
+        const nextCardIdx = prevState
+            .filter((ft) => ft.active === true)
+            .sort((a, b) => (a.pos > b.pos ? 1 : b.pos > a.pos ? -1 : 0))
+            .pop(1).id;
+        // minimize pos
+        prevState.find((f) => f.active === false).pos =
+            Math.min.apply(
+                null,
+                prevState.map(function (o) {
+                    return o.pos;
+                })
+            ) - 1;
+        // reset
+        prevState.find((f) => f.active === false).active = true;
+        // update
+        prevState.find((f) => f.id === nextCardIdx).active = false;
+
+        // update state
+        setCards(prevState);
+    };
+
 
     return (
         <div className={"h-screen w-screen grid place-content-center font-susty"}>
             <>
                 <div className={"text-2xl mx-60 px-1 my-6"}>Suggested searches</div>
-                <div className={"flex flex-row gap-3 mx-10 lg:mx-60"}>
-                    <div className={"z-10 bg-gray-900 bg-opacity-75 bg-transparent rounded-lg rotate-180"}>
+                <div className={"sm:overflow-scroll lg:overflow-hidden flex flex-row gap-3 mx-5 lg:mx-60 cursor-pointer"}>
+                    <div onClick={() => handleLeftClick()}
+                         className={"absolute z-50 bg-gray-900 bg-opacity-75 rounded-lg rotate-180 cursor-pointer"}>
                         <ChevronRightIcon className={"text-white w-5 h-5"}/>
                     </div>
-                    {searches.map((search)=>(
-                        <div className={"px-3 py-1.5 bg-white hover:bg-gray-50 border border-gray-100 shadow-inner-sm rounded-sm hover:cursor-pointer"}>
+                    {searches.filter((f) => f.active === true).sort((a, b) => (a.pos > b.pos ? 1 : b.pos > a.pos ? -1 : 0)).map((search, index) => (
+                        <div key={index}
+                             className={"relative -z-50 px-3 py-1.5 bg-white hover:bg-gray-50 border border-gray-100 shadow-inner-sm rounded-sm"}>
                             <div className={"flex flex-col gap-1"}>
                                 <div className={"font-medium"}>{search.name}</div>
                                 <div className={"text-sm text-gray-600 font-medium"}>{search.views}</div>
                             </div>
                         </div>
                     ))}
-                    <div className={"z-10 bg-gray-900 bg-opacity-75 bg-transparent rounded-lg"}>
+                    <div onClick={() => handleRightClick()}
+                         className={"absolute z-50 bg-gray-900 bg-opacity-75 rounded-lg cursor-pointer"}>
                         <ChevronRightIcon className={"text-white w-5 h-5"}/>
                     </div>
                 </div>
