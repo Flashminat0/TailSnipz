571905bd85af035ed0ad7855f59bdd11ddb40c8e
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2022-05-13 09:36:33 +0530

    header few changes

--- pages/mainheader/profile.js
+++ pages/mainheader/profile.js
@@ -2,6 +2,7 @@ import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useEffect, useRef, useState } from "react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 
+
 function EditInactiveIcon(props) {
   return (
     <svg
@@ -257,124 +258,35 @@ export default function Example() {
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-              <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="px-1 py-1 ">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        className={`${
-                          active ? "bg-violet-500 text-white" : "text-gray-900"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        {active ? (
-                          <EditActiveIcon
-                            className="mr-2 h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        ) : (
-                          <EditInactiveIcon
-                            className="mr-2 h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        )}
-                        Edit
-                      </button>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        className={`${
-                          active ? "bg-violet-500 text-white" : "text-gray-900"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        {active ? (
-                          <DuplicateActiveIcon
-                            className="mr-2 h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        ) : (
-                          <DuplicateInactiveIcon
-                            className="mr-2 h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        )}
-                        Duplicate
-                      </button>
-                    )}
-                  </Menu.Item>
-                </div>
-                <div className="px-1 py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        className={`${
-                          active ? "bg-violet-500 text-white" : "text-gray-900"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        {active ? (
-                          <ArchiveActiveIcon
-                            className="mr-2 h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        ) : (
-                          <ArchiveInactiveIcon
-                            className="mr-2 h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        )}
-                        Archive
-                      </button>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        className={`${
-                          active ? "bg-violet-500 text-white" : "text-gray-900"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        {active ? (
-                          <MoveActiveIcon
-                            className="mr-2 h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        ) : (
-                          <MoveInactiveIcon
-                            className="mr-2 h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        )}
-                        Move
-                      </button>
-                    )}
-                  </Menu.Item>
-                </div>
-                <div className="px-1 py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        className={`${
-                          active ? "bg-violet-500 text-white" : "text-gray-900"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        {active ? (
-                          <DeleteActiveIcon
-                            className="mr-2 h-5 w-5 text-violet-400"
-                            aria-hidden="true"
-                          />
-                        ) : (
-                          <DeleteInactiveIcon
-                            className="mr-2 h-5 w-5 text-violet-400"
-                            aria-hidden="true"
-                          />
-                        )}
-                        Delete
-                      </button>
-                    )}
-                  </Menu.Item>
-                </div>
-              </Menu.Items>
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="">
+              <Menu.Item>
+                  <button
+                    className={"bg-gray-100  text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm"}
+                  >
+                    English
+                  </button>
+              </Menu.Item>
+            </div>
+            <div className="">
+            <Menu.Item>
+                  <button
+                    className={"bg-gray-100  text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm"}
+                  >
+                    Duplicate
+                  </button>
+              </Menu.Item>
+            </div>
+            <div className="">
+            <Menu.Item>
+                  <button
+                    className={"bg-gray-100  text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm"}
+                  >
+                    Duplicate
+                  </button>
+              </Menu.Item>
+            </div>
+          </Menu.Items>
             </Transition>
           </Menu>
         </div>
