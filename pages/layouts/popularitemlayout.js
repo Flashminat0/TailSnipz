73b5c79da805a5ae93a04da28366054c0a78317a
swee76 <oshinikithma1213@gmail.com>
2022-05-20 12:34:35 +0530

    slider in popular items layout finalized

--- pages/layouts/popularitemlayout.js
+++ pages/layouts/popularitemlayout.js
@@ -2,37 +2,6 @@ import React, {useRef} from 'react';
 import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/react/solid";
 import Popularitemcard from "../item/popularitemcard";
 
-const items = [{
-    src: "https://i.pinimg.com/474x/c2/7e/1c/c27e1ccc0b9e3fdf999495e569f19ea5.jpg",
-    alt: "",
-    price: "5.00",
-    size: "12/L",
-    brand:"Shein"
-},{
-    src: "https://i.pinimg.com/474x/c2/7e/1c/c27e1ccc0b9e3fdf999495e569f19ea5.jpg",
-    alt: "",
-    price: "5.00",
-    size: "24/3X",
-    brand:"Torrid"
-},{
-    src: "https://i.pinimg.com/474x/c2/7e/1c/c27e1ccc0b9e3fdf999495e569f19ea5.jpg",
-    alt: "",
-    price: "5.00",
-    size: "24/L",
-    brand:"Shein"
-},{
-    src: "https://i.pinimg.com/474x/c2/7e/1c/c27e1ccc0b9e3fdf999495e569f19ea5.jpg",
-    alt: "",
-    price: "5.00",
-    size: "12/L",
-    brand:"Shein"
-},{
-    src: "https://i.pinimg.com/474x/c2/7e/1c/c27e1ccc0b9e3fdf999495e569f19ea5.jpg",
-    alt: "",
-    price: "5.00",
-    size: "2/XS",
-    brand:"Shein"
-},];
 
 const Popularitemlayout = ({children}) => {
     const ref = useRef(null);
@@ -42,7 +11,10 @@ const Popularitemlayout = ({children}) => {
     };
 
     return (
-        <div className={"px-4 overflow-x-hidden overflow-y-hidden scroll-smooth max-w-lg lg:max-w-full flex flex-row gap-3"} ref={ref}>
+
+        <div
+            className={"px-4 py-4 overflow-x-hidden overflow-y-hidden scroll-smooth min-w-full lg:max-w-full flex flex-row gap-3"}
+            ref={ref}>
             <div className={`lg:hidden absolute -left-4 sm:left-2 lg:left-1/4`}>
                 <div onClick={() => {
                     scroll(-100)
@@ -51,10 +23,9 @@ const Popularitemlayout = ({children}) => {
                     <ChevronLeftIcon className={"text-white w-10 h-10"}/>
                 </div>
             </div>
-                {items.map((items)=>(
-                    <Popularitemcard src={items.src} alt={items.alt} price={items.price} size={items.size} brand={items.brand}/>
-                ))}
-            <div className={"w-56 bg-gray-200 inline-flex items-center place-content-center"}>
+            {children}
+            <div
+                className={"min-w-max lg:w-56 px-8 my-3 lg:my-0 bg-gray-200 inline-flex items-center place-content-center"}>
                 <div className={"text-gray-500"}>See all popular items</div>
             </div>
             <div className={`lg:hidden absolute -right-64 sm:right-2 lg:right-1/4`}>
