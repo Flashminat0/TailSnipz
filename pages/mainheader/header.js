b220b4d9268da38e3dd3c1abf2d50a4206fe1b35
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2022-05-23 10:30:35 +0530

    new layout for header

--- pages/mainheader/header.js
+++ pages/mainheader/header.js
@@ -189,109 +189,110 @@ const Header = () => {
   const [selected, setSelected] = useState(people[3]);
 
   return (
-    <div className={""}>
+    <div className={"px-40"}>
       <>
-        <div className="max-w-full mx-auto px-2 sm:px-6 lg:px-8 ">
-          <div className="relative flex items-center justify-between h-16 pl-6 ml-80 mr-32 pr-40 ">
-            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex-shrink-0 flex items-center mr-8">
-                <Image src="/Susty.png" width={85} height={44} />
-              </div>
-              <div>
-                <div className="mt-1 relative rounded-md shadow-sm border-8">
-                  <input
-                    type="text"
-                    className=" focus:ring-indigo-500 bg-gray-100 
+        <div class="grid grid-flow-col gap-1 ">
+          {/* Logo */}
+          <div class=" col-span-1 border  items-center">
+            <div className=" pl-8">
+              <Image src="/Susty.png" width={85} height={44} />
+            </div>
+          </div>
+          {/* Search */}
+          <div class=" col-span-12 border ">
+            <div className=" pr-8 relative rounded-md shadow-sm border-4 border-susty  w-full">
+              <input
+                type="text"
+                className=" focus:ring-indigo-500 bg-gray-100 
                                 focus:border-indigo-500 block w-full pl-36 
                                 pr-40 ml-4  sm:text-sm border-gray-300 rounded-md"
-                    placeholder="Search for items"
-                  />
-                  <div className="absolute inset-y-0 left-0 pl-1 flex items-center">
-                    <Listbox value={selected} onChange={setSelected}>
-                      {({ open }) => (
-                        <>
-                          <div className="ml-3 relative">
-                            <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                              <span className="flex items-center">
-                                <span className="ml-3 block truncate">
-                                  {selected.name}
-                                </span>
-                              </span>
-                              <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                <SelectorIcon
-                                  className="h-5 w-5 text-gray-400"
-                                  aria-hidden="true"
-                                />
-                              </span>
-                            </Listbox.Button>
+                placeholder="Search for items"
+              />
+              <div className="absolute inset-y-0 left-0 pl-1 flex items-center">
+                <Listbox value={selected} onChange={setSelected}>
+                  {({ open }) => (
+                    <>
+                      <div className="ml-3 relative">
+                        <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                          <span className="flex items-center">
+                            <span className="ml-3 block truncate">
+                              {selected.name}
+                            </span>
+                          </span>
+                          <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                            <SelectorIcon
+                              className="h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </Listbox.Button>
 
-                            <Transition
-                              show={open}
-                              as={Fragment}
-                              leave="transition ease-in duration-100"
-                              leaveFrom="opacity-100"
-                              leaveTo="opacity-0"
-                            >
-                              <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                                {people.map((person) => (
-                                  <Listbox.Option
-                                    key={person.id}
-                                    className={({ active }) =>
-                                      classNames(
-                                        active
-                                          ? "text-white bg-indigo-600"
-                                          : "text-gray-900",
-                                        "cursor-default select-none relative py-2 pl-3 pr-9"
-                                      )
-                                    }
-                                    value={person}
-                                  >
-                                    {({ selected, active }) => (
-                                      <>
-                                        <div className="flex items-center">
-                                          <span
-                                            className={classNames(
-                                              selected
-                                                ? "font-semibold"
-                                                : "font-normal",
-                                              "ml-3 block truncate"
-                                            )}
-                                          >
-                                            {person.name}
-                                          </span>
-                                        </div>
+                        <Transition
+                          show={open}
+                          as={Fragment}
+                          leave="transition ease-in duration-100"
+                          leaveFrom="opacity-100"
+                          leaveTo="opacity-0"
+                        >
+                          <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                            {people.map((person) => (
+                              <Listbox.Option
+                                key={person.id}
+                                className={({ active }) =>
+                                  classNames(
+                                    active
+                                      ? "text-white bg-indigo-600"
+                                      : "text-gray-900",
+                                    "cursor-default select-none relative py-2 pl-3 pr-9"
+                                  )
+                                }
+                                value={person}
+                              >
+                                {({ selected, active }) => (
+                                  <>
+                                    <div className="flex items-center">
+                                      <span
+                                        className={classNames(
+                                          selected
+                                            ? "font-semibold"
+                                            : "font-normal",
+                                          "ml-3 block truncate"
+                                        )}
+                                      >
+                                        {person.name}
+                                      </span>
+                                    </div>
 
-                                        {selected ? (
-                                          <span
-                                            className={classNames(
-                                              active
-                                                ? "text-white"
-                                                : "text-indigo-600",
-                                              "absolute inset-y-0 right-0 flex items-center pr-4"
-                                            )}
-                                          >
-                                            <CheckIcon
-                                              className="h-5 w-5"
-                                              aria-hidden="true"
-                                            />
-                                          </span>
-                                        ) : null}
-                                      </>
-                                    )}
-                                  </Listbox.Option>
-                                ))}
-                              </Listbox.Options>
-                            </Transition>
-                          </div>
-                        </>
-                      )}
-                    </Listbox>
-                  </div>
-                </div>
+                                    {selected ? (
+                                      <span
+                                        className={classNames(
+                                          active
+                                            ? "text-white"
+                                            : "text-indigo-600",
+                                          "absolute inset-y-0 right-0 flex items-center pr-4"
+                                        )}
+                                      >
+                                        <CheckIcon
+                                          className="h-5 w-5"
+                                          aria-hidden="true"
+                                        />
+                                      </span>
+                                    ) : null}
+                                  </>
+                                )}
+                              </Listbox.Option>
+                            ))}
+                          </Listbox.Options>
+                        </Transition>
+                      </div>
+                    </>
+                  )}
+                </Listbox>
               </div>
             </div>
-
-            {/** Messages */}
+          </div>
+          {/* mail */}
+          <div class=" col-span-1 border ">
             <div className="">
               <Menu as="div" className="relative inline-block text-left">
                 <div>
@@ -359,10 +360,11 @@ const Header = () => {
                 </Transition>
               </Menu>
             </div>
-            {/** End Messages */}
-
-            {/** Notification */}
-            <Menu as="div" className="relative inline-block text-left">
+          </div>
+          {/* notification */}
+          <div class=" col-span-1 border ">
+            {" "}
+            <Menu as="div" className="relative inline-block text-left ">
               <div>
                 <Menu.Button className="inline-flex justify-center w-full   px-4 py-2 bg-white text-sm font-medium text-gray-700   focus:ring-indigo-500">
                   <svg
@@ -494,9 +496,10 @@ const Header = () => {
                 </Menu.Items>
               </Transition>
             </Menu>
-            {/**End Notification */}
+          </div>
 
-            {/*Favorite*/}
+          {/*Favotite*/}
+          <div class=" col-span-1 ... border ">
             <div className="">
               <Menu as="div" className="relative inline-block text-left">
                 <div>
@@ -564,9 +567,10 @@ const Header = () => {
                 </Transition>
               </Menu>
             </div>
-            {/* End Favorite */}
-
-            {/** Profile */}
+          </div>
+          {/* Profile */}
+          <div class=" col-span-1 border">
+            {" "}
             <Menu as="div" className="relative inline-block text-left">
               <div>
                 <Menu.Button className="inline-flex justify-center w-full px-1 py-1 bg-white text-sm font-medium text-gray-400 focus:ring-indigo-500">
@@ -630,18 +634,20 @@ const Header = () => {
                 </Menu.Items>
               </Transition>
             </Menu>
-            {/** End Profile */}
-
-            {/** Sell Now*/}
+          </div>
+          {/* Sell now */}
+          <div class=" col-span-1 ... border">
+            {" "}
             <button
               type="button"
               class="text-white bg-susty  mt-1  font-md rounded-lg text-sm px-2 py-1 mr-2 mb-2"
             >
               Sell now
             </button>
-            {/** End Sell Now */}
-
-            {/** Question */}
+          </div>
+          {/* Question mark */}
+          <div class=" col-span-1 border">
+            {" "}
             <div className="">
               <Menu as="div" className="relative inline-block text-left">
                 <div>
@@ -709,9 +715,10 @@ const Header = () => {
                 </Transition>
               </Menu>
             </div>
-            {/** End Question */}
-
-            {/** Language */}
+          </div>
+          {/* Language */}
+          <div class=" col-span-1 border ">
+            {" "}
             <div className="">
               <Menu as="div" className="relative inline-block text-left">
                 <div>
@@ -778,11 +785,12 @@ const Header = () => {
                 </Transition>
               </Menu>
             </div>
-            {/** End Language */}
           </div>
+        </div>
 
+        <div className="max-w-full mx-auto px-2 sm:px-6 lg:px-8 ">
           <row
-            className="bg-white w-full ml-80 pl-10 "
+            className="bg-white w-full ml-2 pl-6 "
             style={{ position: "fixed", border: 0 }}
           >
             <div offset={2}>
