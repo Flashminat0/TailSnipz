d64b1434190e79a9bec0d95381adefc83719b4d4
swee76 <oshinikithma1213@gmail.com>
2022-05-23 15:49:41 +0530

    Bugs fixed in referralswork.js

--- pages/referrals/referralswork.js
+++ pages/referrals/referralswork.js
@@ -2,50 +2,51 @@ import React, {Fragment, useState} from 'react';
 import Link from "next/link";
 import {ChevronRightIcon} from "@heroicons/react/outline";
 import {AnimatePresence, motion} from "framer-motion";
-import {Dialog, Listbox, Transition} from "@headlessui/react";
-import {BookmarkAltIcon, CheckIcon, SelectorIcon} from "@heroicons/react/solid";
+import {Dialog} from "@headlessui/react";
 
-const img1 = 'https://5a2583d7dd16c25cb2e8-358d15e499fca729302e63598be13736.ssl.cf3.rackcdn.com/referrals/referrals_share.png';
-const img2 = 'https://5a2583d7dd16c25cb2e8-358d15e499fca729302e63598be13736.ssl.cf3.rackcdn.com/referrals/referrals_friends.png';
-const img3 = 'https://5a2583d7dd16c25cb2e8-358d15e499fca729302e63598be13736.ssl.cf3.rackcdn.com/referrals/referrals_voucher.png';
+const imageStaticList = [{
+    id: 1,
+    src: "https://5a2583d7dd16c25cb2e8-358d15e499fca729302e63598be13736.ssl.cf3.rackcdn.com/referrals/referrals_share.png",
+    alt: "Envelope Image",
+    topic: "Share your link",
+    description: "Invite people to join Vinted by sharing your link with them."
+}, {
+    id: 2,
+    src: "https://5a2583d7dd16c25cb2e8-358d15e499fca729302e63598be13736.ssl.cf3.rackcdn.com/referrals/referrals_friends.png",
+    alt: "Mobile Image",
+    topic: "Tell your friends to list",
+    description: "When they list 3 items within 7 days of joining, you’ll get a $5 voucher. You’ll get an extra $10 voucher if they complete a sale within 30 days."
+}, {
+    id: 3,
+    src: "https://5a2583d7dd16c25cb2e8-358d15e499fca729302e63598be13736.ssl.cf3.rackcdn.com/referrals/referrals_voucher.png",
+    alt: "Shoe Image",
+    topic: "Spend your voucher",
+    description: "Buy an item that costs $15 or more (not including shipping), and your voucher will be automatically deducted from the item price."
+},];
+
 const mobileImg = 'https://5a2583d7dd16c25cb2e8-358d15e499fca729302e63598be13736.ssl.cf3.rackcdn.com/referrals/how_it_works_icon.png';
 
 const Referralswork = () => {
     const [openReferralsModal, setOpenReferralsModal] = useState(false);
+    const [imageList, setImageList] = useState(imageStaticList);
 
     return (
         <div className={"h-screen w-screen grid place-content-center font-susty"}>
             <>
                 <div className={"hidden md:block"}>
-                    <div className={"grid grid-rows-6 gap-1 px-10"}>
+                    <div className={"grid grid-rows-6 gap-1 px-8"}>
                         <div className={"row-span-1 text-3xl"}>How referrals work</div>
                         <div className={"row-span-4 grid grid-cols-3 gap-8"}>
-                            <div className={"col-span-1 col-start-1 my-5"}>
-                                <img src={img1} alt={""} className={"object-cover box-content"} width={"300"}
-                                     height={"300"}/>
-                                <div className={"text-2xl font-medium my-2"}>Share your link</div>
-                                <div className={"text-gray-500 font-medium"}>Invite people to join Vinted by sharing
-                                    your link with them.
+                            {imageList.map((image)=>(
+                                <div key={image.id}>
+                                    <div className={"col-span-1 my-5"}>
+                                        <img src={image.src} alt={image.alt} className={"object-cover box-content"} width={"300"}
+                                             height={"300"}/>
+                                        <div className={"text-2xl font-medium my-2"}>{image.topic}</div>
+                                        <div className={"text-sm text-gray-500 font-medium"}>{image.description}</div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className={"col-span-1 col-start-2 my-5"}>
-                                <img src={img2} alt={""} className={"object-cover box-content"} width={"300"}
-                                     height={"300"}/>
-                                <div className={"text-2xl font-medium my-2"}>Tell your friends to list</div>
-                                <div className={"text-gray-500 font-medium"}>When they list 3 items within 7 days of
-                                    joining, you’ll get a $5 voucher. You’ll get an extra $10 voucher if they complete a
-                                    sale within 30 days.
-                                </div>
-                            </div>
-                            <div className={"col-span-1 col-start-3  my-5"}>
-                                <img src={img3} alt={""} className={"object-cover box-content"} width={"300"}
-                                     height={"300"}/>
-                                <div className={"text-2xl font-medium my-2"}>Spend your voucher</div>
-                                <div className={"text-gray-500 font-medium"}>Buy an item that costs $15 or more (not
-                                    including shipping), and your voucher will be automatically deducted from the item
-                                    price.
-                                </div>
-                            </div>
+                            ))}
                         </div>
                         <div className={"row-span-1 text-xs text-gray-500"}>
                             This offer is subject to <Link href={"/"}><a className={"text-susty hover:underline"}>terms
@@ -112,7 +113,7 @@ const Referralswork = () => {
                                                         as="h3"
                                                         className={`mb-5`}
                                                     >
-                                                        <p className="text-base font-medium leading-5 flex justify-between ml-28">
+                                                        <p className="text-base font-medium leading-5 flex justify-between ml-10 sm:ml-28">
                                                             How referrals work
                                                             <span
                                                                 onClick={() => setOpenReferralsModal(false)}
@@ -124,53 +125,19 @@ const Referralswork = () => {
                                                     </Dialog.Title>
 
 
-                                                    <div className={"grid grid-rows-6 gap-y-8"}>
-                                                        <div className={"row-span-2"}>
-                                                            <img src={img1} alt={""}
-                                                                 className={"mx-auto object-cover box-content"}
-                                                                 width={"200"}
-                                                                 height={"200"}/>
-                                                            <div className={"text-base font-medium my-2"}>Share your
-                                                                link
+                                                    <div className={"grid grid-rows-3 gap-y-8"}>
+                                                        {imageList.map((image)=>(
+                                                            <div key={image.id}>
+                                                                <div className={"row-span-2"}>
+                                                                    <img src={image.src} alt={image.alt}
+                                                                         className={"mx-auto object-cover box-content"}
+                                                                         width={"200"}
+                                                                         height={"200"}/>
+                                                                    <div className={"text-base font-medium my-2"}>{image.topic}</div>
+                                                                    <div className={"text-sm text-gray-500 font-medium"}>{image.description}</div>
+                                                                </div>
                                                             </div>
-                                                            <div className={"text-sm text-gray-500 font-medium"}>Invite
-                                                                people
-                                                                to join Vinted by sharing
-                                                                your link with them.
-                                                            </div>
-                                                        </div>
-                                                        <div className={"row-span-2"}>
-                                                            <img src={img2} alt={""}
-                                                                 className={"mx-auto object-cover box-content"}
-                                                                 width={"200"}
-                                                                 height={"200"}/>
-                                                            <div className={"text-base font-medium my-2"}>Tell your
-                                                                friends to list
-                                                            </div>
-                                                            <div className={"text-sm text-gray-500 font-medium"}>When
-                                                                they list
-                                                                3 items within 7 days of
-                                                                joining, you’ll get a $5 voucher. You’ll get an extra
-                                                                $10 voucher if they complete a
-                                                                sale within 30 days.
-                                                            </div>
-                                                        </div>
-                                                        <div className={"row-span-2"}>
-                                                            <img src={img3} alt={""}
-                                                                 className={"mx-auto object-cover box-content"}
-                                                                 width={"200"}
-                                                                 height={"200"}/>
-                                                            <div className={"text-base font-medium my-2"}>Spend your
-                                                                voucher
-                                                            </div>
-                                                            <div className={"text-sm text-gray-500 font-medium"}>Buy an
-                                                                item
-                                                                that costs $15 or more (not
-                                                                including shipping), and your voucher will be
-                                                                automatically deducted from the item
-                                                                price.
-                                                            </div>
-                                                        </div>
+                                                        ))}
                                                     </div>
 
                                                 </motion.div>
