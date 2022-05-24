6781fc13c4ee4fc7324491db8537eb46eccfd169
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2022-05-24 10:04:48 +0530

    Update productdetails.js

--- pages/productdetails/productdetails.js
+++ pages/productdetails/productdetails.js
@@ -8,29 +8,60 @@ const productdetails = () => {
           {/* left side details */}
           <div class="col-span-4 border">
             {/* Product Image set */}
-            <div class="grid grid-rows-3 grid-flow-col">
-              <div class="row-span-3 border">
-                <img
-                  className="object-fill h-200 w-196"
-                  src="https://picsum.photos/500/550"
-                  alt="Bonnie image"
-                />
+
+            <section class="overflow-hidden text-gray-700">
+              <div class="container px-5 py-2 mx-auto">
+                <div class="flex flex-wrap -m-1 md:-m-2">
+                  <div class="flex flex-wrap w-1/2">
+                    <div class="w-1/2  ">
+                      <img
+                        alt="gallery"
+                        class="block object-cover object-center w-full h-full "
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
+                      />
+                    </div>
+                    <div class="w-1/2  ">
+                      <img
+                        alt="gallery"
+                        class="block object-cover object-center w-full h-full "
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+                      />
+                    </div>
+                    <div class="w-full  ">
+                      <img
+                        alt="gallery"
+                        class="block object-cover object-center w-full h-full "
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                      />
+                    </div>
+                  </div>
+                  <div class="flex flex-wrap w-1/2">
+                    <div class="w-full  ">
+                      <img
+                        alt="gallery"
+                        class="block object-cover object-center w-full h-full "
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
+                      />
+                    </div>
+                    <div class="w-1/2  ">
+                      <img
+                        alt="gallery"
+                        class="block object-cover object-center w-full h-full "
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
+                      />
+                    </div>
+                    <div class="w-1/2  ">
+                      <img
+                        alt="gallery"
+                        class="block object-cover object-center w-full h-full "
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div class="col-span-2 border">
-                <img
-                  className="object-fill h-100 w-196"
-                  src="https://picsum.photos/400/250"
-                  alt="Bonnie image"
-                />
-              </div>
-              <div class="row-span-2 col-span-2 border">
-                <img
-                  className="object-fill h-100 w-196"
-                  src="https://picsum.photos/400/250"
-                  alt="Bonnie image"
-                />
-              </div>
-            </div>
+            </section>
+
             {/* End Product Image set */}
             {/* Navigation and report  */}
             <div className="">
