533602da7177bb8bd3101bf57210d1b7d310fb1c
swee76 <oshinikithma1213@gmail.com>
2022-05-25 16:25:27 +0530

    create order mobile table

---
+++ pages/layouts/mobiletablelayout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Mobiletablelayout = ({children}) => {
+    return (
+        <div className={"px-5 py-6"}>
+            <div className="flex flex-col ">
+                <div className="shadow-lg overflow-hidden border-b border-gray-200">
+                    {children}
+                    {/*TODO NAVIGATION DYNAMIC */}
+                    <div
+                        className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+                        <div className="flex-1 flex justify-between sm:hidden">
+                            <a
+                                href="#"
+                                className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                            >
+                                Previous
+                            </a>
+                            <a
+                                href="#"
+                                className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                            >
+                                Next
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Mobiletablelayout;
