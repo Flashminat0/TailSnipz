c32a6eac14257755b422fdd144d4d1b892284611
swee76 <oshinikithma1213@gmail.com>
2022-05-25 12:44:38 +0530

    create newsfeed in home page

--- pages/item/popularitemcard.js
+++ pages/item/popularitemcard.js
@@ -8,25 +8,25 @@ const Popularitemcard = ({src, alt, price, size, brand}) => {
     return (
 
         <>
-            <div className={"hidden lg:block box-content w-56 min-h-max"}>
-                <div><img className={"h-80 object-fit"}
+            <div className={"hidden lg:block box-content w-[12rem] min-h-max"}>
+                <div><img className={"h-[20rem] w-[12rem] object-cover"}
                           src={src} alt={alt}
                           width={"full"} height={"full"}/></div>
                 <div className={"p-3 grid grid-cols-3"}>
                     <div className={"col-start-1 col-end-3"}>
-                        <div className={"flex flex-col gap-1"}>
+                        <div className={"flex flex-col gap-0.5"}>
                             <div className={"flex flex-row gap-1"}>
                                 <div className={"font-semibold text-sm"}>${price}</div>
-                                <InformationCircleIcon className={"w-6 h-6 text-gray-500 text-sm"}/>
+                                <InformationCircleIcon className={"w-[1.25rem] h-[1.25rem] text-gray-500 text-sm"}/>
                             </div>
-                            <div className={"text-gray-500 text-sm"}>{size}</div>
-                            <div className={"text-gray-500 text-sm"}>{brand}</div>
+                            <div className={"text-gray-500 text-xs"}>{size}</div>
+                            <div className={"text-gray-500 text-xs"}>{brand}</div>
                         </div>
                     </div>
                     <div className={"col-start-3 col-end-4"}>
                         <div className={"flex flex-row justify-end items-center gap-0.5 text-gray-400"}>
                             {isFavourite === true ? <>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-900"
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-red-900"
                                      viewBox="0 0 20 20" onClick={() => {
                                     setIsFavourite(false)
                                 }}
@@ -36,7 +36,7 @@ const Popularitemcard = ({src, alt, price, size, brand}) => {
                                           clipRule="evenodd"/>
                                 </svg>
                             </> : <>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
                                      onClick={() => {
                                          setIsFavourite(true)
                                      }}
@@ -96,8 +96,8 @@ const Popularitemcard = ({src, alt, price, size, brand}) => {
                                     </div>
                                 </div>
                             </div>
-                            <div className={"text-gray-500 text-sm"}>{size}</div>
-                            <div className={"text-gray-500 text-sm"}>{brand}</div>
+                            <div className={"text-gray-500 text-xs"}>{size}</div>
+                            <div className={"text-gray-500 text-xs"}>{brand}</div>
                         </div>
                     </div>
                 </div>
--- pages/item/popularitems.js
+++ pages/item/popularitems.js
@@ -4,20 +4,23 @@ import Popularitemlayout from "../layouts/popularitemlayout";
 
 const itemsStaticData = [{
     id: 1,
-    src: "https://i.pinimg.com/474x/c2/7e/1c/c27e1ccc0b9e3fdf999495e569f19ea5.jpg",
+    username:"NickyMontana",
+    src: "https://cdn.shopify.com/s/files/1/0059/5879/5337/products/handknitted-butterfly-jacket-by-hanne-falkenberg-design-andet-hanne-falkenberg-kit-6-m-992013_620x.jpg?v=1631370781",
     alt: "frock picture",
     price: "5.00",
     size: "12/L",
     brand: "Shein"
 }, {
     id: 2,
-    src: "https://i.pinimg.com/474x/c2/7e/1c/c27e1ccc0b9e3fdf999495e569f19ea5.jpg",
+    username:"Pedro123",
+    src: "https://images.pexels.com/photos/19090/pexels-photo.jpg?cs=srgb&dl=pexels-web-donut-19090.jpg&fm=jpg",
     alt: "frock picture",
     price: "5.00",
     size: "24/3X",
     brand: "Torrid"
 }, {
     id: 3,
+    username:"LeonZucchini",
     src: "https://i.pinimg.com/474x/c2/7e/1c/c27e1ccc0b9e3fdf999495e569f19ea5.jpg",
     alt: "frock picture",
     price: "5.00",
@@ -25,6 +28,7 @@ const itemsStaticData = [{
     brand: "Shein"
 }, {
     id: 4,
+    username:"IrynaSuprun",
     src: "https://i.pinimg.com/474x/c2/7e/1c/c27e1ccc0b9e3fdf999495e569f19ea5.jpg",
     alt: "frock picture",
     price: "5.00",
@@ -32,6 +36,7 @@ const itemsStaticData = [{
     brand: "Shein"
 }, {
     id: 5,
+    username:"Lauren32",
     src: "https://i.pinimg.com/474x/c2/7e/1c/c27e1ccc0b9e3fdf999495e569f19ea5.jpg",
     alt: "frock picture",
     price: "5.00",
