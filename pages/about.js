24966d14c62900c8c8c22c78e001210c4cb73de7
swee76 <oshinikithma1213@gmail.com>
2022-05-25 18:59:27 +0530

    bugs fixed 2

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {useRouter} from "next/router";
+import Ordermobiletablepage from "../components/ordertable/ordertableseller/ordermobiletablepage";
 
 const About = () => {
     const router = useRouter();
 
     return (
         <div>
-
+            <Ordermobiletablepage/>
         </div>
     );
 };
