5edeb41793d24680b57eaeabd1dc950ed37b4bec
swee76 <oshinikithma1213@gmail.com>
2022-06-06 17:26:43 +0530

    Bug Fixed Final Home Page 3

--- components/rates/RatesInClosetSpotlight.jsx
+++ components/rates/RatesInClosetSpotlight.jsx
@@ -4,11 +4,11 @@ import {StarIcon} from "@heroicons/react/solid";
 const RatesInClosetSpotlight = ({person, personD, id}) => {
     return (
         <div className={'flex flex-row items-center'} key={id}>
-            {Array(Math.ceil(parseFloat(personD.rating))).fill(0).map(() => {
-                return <StarIcon className={'w-5 h-5 text-amber-400'}/>
+            {Array(Math.ceil(parseFloat(personD.rating))).fill(0).map((x, index) => {
+                return <StarIcon key={index} className={'w-5 h-5 text-amber-400'}/>
             })}
-            {Array(Math.floor(5 - parseFloat(personD.rating))).fill(0).map(() => {
-                return <StarIcon className={'w-5 h-5 text-gray-300'}/>
+            {Array(Math.floor(5 - parseFloat(personD.rating))).fill(0).map((x, index) => {
+                return <StarIcon key={index} className={'w-5 h-5 text-gray-300'}/>
             })}
             <p className={'pl-2 pb-1 text-base text-gray-500'}>
                 {person.length}
