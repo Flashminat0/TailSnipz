a889a2bf83e977d891c59375169977b5ff1c473a
Flashminat0 <wyasaslowe@gmail.com>
2022-06-07 15:51:35 +0530

    Header fixup

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.prepare()
         });
 
 
-        server.listen(3001, (err) => {
+        server.listen(3000, (err) => {
             if (err) throw err;
-            console.log("> Ready on http://localhost:3001");
+            console.log("> Ready on http://localhost:3000");
         });
     })
     .catch((err) => {
