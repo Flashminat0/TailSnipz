095b27b497aa14fa0fd0e9acbcd46d4f01d32f7e
swee76 <oshinikithma1213@gmail.com>
2022-06-07 16:29:45 +0530

    Changes in Footer section

--- components/footer/FooterSection.jsx
+++ components/footer/FooterSection.jsx
@@ -3,12 +3,11 @@ import Link from "next/link";
 
 const FooterSection = () => {
     return (
-        <div className={'flex flex-row gap-4'}>
+        <div className={'flex flex-row gap-4 my-3'}>
             <Link href={'/footer/policy'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Privacy Policy</a></Link>
             <Link href={'/footer/cookiepolicy'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Cookie Policy</a></Link>
             <Link href={'#'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Cookie Settings</a></Link>
             <Link href={'/footer/termsandconditions'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Terms & Conditions</a></Link>
-            
         </div>
     );
 };
