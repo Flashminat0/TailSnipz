98666f80d1564442310e9cf24dad4450056a5a62
swee76 <oshinikithma1213@gmail.com>
2022-06-07 18:55:51 +0530

    Tried to fix header section in main header folder of components

--- components/footer/FooterSection.jsx
+++ components/footer/FooterSection.jsx
@@ -6,7 +6,7 @@ const FooterSection = () => {
         <div className={'flex flex-row gap-4 my-3'}>
             <Link href={'/footer/policy'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Privacy Policy</a></Link>
             <Link href={'/footer/cookiepolicy'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Cookie Policy</a></Link>
-            <Link href={'#'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Cookie Settings</a></Link>
+            <Link href={'/footer/privacy'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Cookie Settings</a></Link>
             <Link href={'/footer/termsandconditions'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Terms & Conditions</a></Link>
         </div>
     );
