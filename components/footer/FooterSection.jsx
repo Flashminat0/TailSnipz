8f90a0cd09b9561cf19b9cb6034a43c3c1da3976
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2022-06-07 15:14:25 +0530

    cookies policy changed

--- components/footer/FooterSection.jsx
+++ components/footer/FooterSection.jsx
@@ -5,7 +5,7 @@ const FooterSection = () => {
     return (
         <div className={'flex flex-row gap-4'}>
             <Link href={'/footer/policy'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Privacy Policy</a></Link>
-            <Link href={'#'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Cookie Policy</a></Link>
+            <Link href={'/footer/cookiepolicy'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Cookie Policy</a></Link>
             <Link href={'#'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Cookie Settings</a></Link>
             <Link href={'#'}><a className={'text-sm text-gray-500 font-semibold hover:underline'}>Terms & Conditions</a></Link>
 
