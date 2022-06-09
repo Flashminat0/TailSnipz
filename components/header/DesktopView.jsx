82b745220c276ab2c464903d41f048c7de10992c
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-06-09 14:58:15 +0530

    some changes added, alignment and overflow

--- components/header/DesktopView.jsx
+++ components/header/DesktopView.jsx
@@ -91,14 +91,16 @@ const userDropdown = [
 ]
 
 const languageDropdown = [
-    {short: "EN", long: "English, US(English)"},
-    {short: "US", long: "Español, US(Spanish)"}
+    {id: "423f9b07-1d8d-4b79-bd9a-0038dc0fb94a" ,short: "EN", long: "English, US(English)"},
+    {id: "42eb2a91-6c77-4f24-a233-e1ca1ec27f07" ,short: "US", long: "Español, US(Spanish)"}
 ]
 
 const DesktopView = () => {
     const [searchBarOptions, setSearchBarOptions] = useState(searchBarStaticOptions);
     const [guestMobileArray, setGuestMobileArray] = useState(guestMobileArrayStaticData);
     const [catalogMobileArray, setCatalogMobileArray] = useState(catalogMobileArrayStaticData);
+    const [userActivityArray, setUserActivityArray] = useState(userDropdown)
+    const [languageArray, setLanguageArray] = useState(languageDropdown)
     const [selectedLanguage, setSelectedLanguage] = useState("EN")
 
     const router = useRouter()
@@ -114,25 +116,19 @@ const DesktopView = () => {
         <>
             <Popover className="relative ">
                 <div className="flex justify-between items-center">
-                    <div className="flex justify-start">
+                    <div className="flex justify-start mb-1">
                         <a href="#">
                             <span className="sr-only">Workflow</span>
                             <img
-                                className="h-8 w-auto sm:h-10"
+                                className="h-8 min-w-fit sm:h-10"
                                 src="https://firebasestorage.googleapis.com/v0/b/susty-next.appspot.com/o/Susty.png?alt=media&token=08d4c7c6-fcda-4381-bbe6-4ea5c9eb5b85"
                                 alt="Image"
                             />
                         </a>
                     </div>
 
-                    {/* <div className="-mr-2 -my-2 md:hidden ">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Open menu</span>
-                        <HiMenu className="h-6 w-6" aria-hidden="true" />
-                    </Popover.Button>
-                </div> */}
-
-                    <div className={'hidden md:block w-full'}>
+                    <div className={'hidden md:block w-full pl-4'}>
+                        
                         <SearchBar
                             selectedSearchBarOption={selectedSearchBarOption}
                             setSelectedSearchBarOption={setSelectedSearchBarOption}
@@ -144,21 +140,21 @@ const DesktopView = () => {
                         {
                             loggedIn && (
                                 <>
-                                    <div className='md:-ml-4 lg:-ml-4'>
+                                    <div className=' mr-1'>
                                         <AiOutlineMail color="#a3a3a3" className='w-6 h-6'/>
                                     </div>
-                                    <div>
+                                    <div className='mx-1'>
                                        <IoNotificationsOutline color="#a3a3a3" className='w-6 h-6'/>
                                     </div>
-                                    <div>
+                                    <div className='mx-1'>
                                         <AiOutlineHeart color="#a3a3a3" className='w-6 h-6'/>
                                     </div>
 
-                                    <Menu as="div" className="relative inline-block text-left">
+                                    <Menu as="div" className="relative inline-block text-left mx-1 hidden md:block">
                                         <div>
                                             <Menu.Button className="inline-flex justify-center -my-1 pl-2 md:pl-0 md:mr-8 md:pr-2 w-full rounded-md ring-0 text-sm font-medium text-gray-700">
                                                 <img alt='User' className='rounded-full' height="30" width="30" src='https://firebasestorage.googleapis.com/v0/b/susty-next.appspot.com/o/default_profile_pic.png?alt=media&token=ddfbe30b-a94f-4390-94cf-416285ac2fde' />
-                                                <ChevronDownIcon className=" ml-0 h-8 w-3" aria-hidden="true" />
+                                                <ChevronDownIcon color='#a3a3a3' className=" ml-0.5 h-8 w-3" aria-hidden="true" />
                                             </Menu.Button>
                                         </div>
 
@@ -175,7 +171,7 @@ const DesktopView = () => {
                                             <div className="py-1">
 
                                             {
-                                                userDropdown.map((item, idx) => {
+                                                userActivityArray.map((item, idx) => {
                                                     return (
                                                         <Menu.Item key={item.id}>
                                                             {({ active }) => (
@@ -202,51 +198,6 @@ const DesktopView = () => {
                             )
                         }
 
-                        <Menu as="div" className="md:hidden relative inline-block text-left">
-                            <div>
-                                <Menu.Button className="inline-flex justify-center -my-1 pl-2 md:pl-0 md:mr-8 md:pr-2 w-full rounded-md ring-0 text-sm font-medium text-gray-700">
-                                    <div>{selectedLanguage}</div>
-                                    <ChevronDownIcon className=" ml-0 h-5 w-3" aria-hidden="true" />
-                                </Menu.Button>
-                            </div>
-
-                            <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-100"
-                                enterFrom="transform opacity-0 scale-95"
-                                enterTo="transform opacity-100 scale-100"
-                                leave="transition ease-in duration-75"
-                                leaveFrom="transform opacity-100 scale-100"
-                                leaveTo="transform opacity-0 scale-95"
-                            >
-                                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none overflow-visible z-50">
-                                <div className="py-1">
-
-                                {
-                                    languageDropdown.map((item, idx) => {
-                                        return (
-                                            <Menu.Item key={item.id}>
-                                                {({ active }) => (
-                                                    <a
-                                                    onClick={() => setSelectedLanguage(item.short)}
-                                                    className={classNames(
-                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}
-                                                    >
-                                                        {item.long}
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        )
-                                    })
-                                }
-
-                                </div>
-                                </Menu.Items>
-                            </Transition>
-                        </Menu>
-
                         <div className="-mr-2 -my-2 md:hidden">
                             <Popover.Button
                                 className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-400">
@@ -263,21 +214,21 @@ const DesktopView = () => {
                                 await router.push('/items/add')
                             }}
                             type="button"
-                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-susty hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                            className="inline-flex items-center px-2 py-1.5 mx-1 border border-transparent text-sm leading-4 font-normal rounded-md shadow-sm text-white bg-susty hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                         >
                             Sell&nbsp;now
                         </button>
                     </div>
 
-                    <div className='hidden md:block mx-2'>
+                    <div className='hidden md:block mx-3'>
                         <AiOutlineQuestionCircle color="#a3a3a3" className='h-6 w-6'/>
                     </div>
 
-                    <Menu as="div" className="hidden md:block relative inline-block text-left">
+                    <Menu as="div" className="hidden md:block relative inline-block text-left mx-1">
                         <div>
                             <Menu.Button className="inline-flex justify-center -my-1 pl-2 md:pl-0 md:mr-0 md:pr-0 w-full rounded-md ring-0 text-sm font-medium text-gray-700">
                                 <div>{selectedLanguage}</div>
-                                <ChevronDownIcon className=" ml-0 h-5 w-3" aria-hidden="true" />
+                                <ChevronDownIcon color='#a3a3a3' className=" ml-0.5 h-5 w-3" aria-hidden="true" />
                             </Menu.Button>
                         </div>
 
@@ -294,7 +245,7 @@ const DesktopView = () => {
                             <div className="py-1">
 
                             {
-                                languageDropdown.map((item, idx) => {
+                                languageArray.map((item, idx) => {
                                     return (
                                         <Menu.Item key={item.id}>
                                             {({ active }) => (
@@ -319,7 +270,7 @@ const DesktopView = () => {
                     </Menu>
 
                 </div>
-                <div className={'md:hidden block '}>
+                <div className={'md:hidden block mt-1'}>
                     <SearchBar
                         selectedSearchBarOption={selectedSearchBarOption}
                         setSelectedSearchBarOption={setSelectedSearchBarOption}
@@ -344,13 +295,6 @@ const DesktopView = () => {
                             className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                             <div className="pt-5 pb-6 px-5">
                                 <div className="flex items-center justify-end">
-                                    {/* <div>
-                                    <img
-                                        className="h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                        alt="Workflow"
-                                    />
-                                </div> */}
                                     <div className="-mr-2">
                                         <Popover.Button
                                             className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-400">
