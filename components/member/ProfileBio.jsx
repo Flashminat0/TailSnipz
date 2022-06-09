111b507582cd205a3977d74d37696d067f12993c
swee76 <oshinikithma1213@gmail.com>
2022-06-09 14:05:09 +0530

    optimize imports

--- components/member/ProfileBio.jsx
+++ components/member/ProfileBio.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import {motion} from "framer-motion";
 import {
     CheckCircleIcon,
-    ChevronRightIcon,
     ClockIcon,
     LocationMarkerIcon,
     PencilIcon,
