34581ef68e435e30706a7cc0815b6622ce30fefc
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-06-10 14:52:52 +0530

    header added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
+import DesktopView from '../components/header/DesktopView'
 import '../styles/globals.css'
 
 export default function MyApp({Component, pageProps}) {
     return (
         <>
+            <DesktopView/>
             <Component {...pageProps} />
         </>
     )
