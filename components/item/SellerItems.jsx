723b1dee8f7357587cae205c980e182bf859ed5b
swee76 <oshinikithma1213@gmail.com>
2022-06-13 19:34:31 +0530

    bug fixed in SellerItems.jsx

--- components/item/SellerItems.jsx
+++ components/item/SellerItems.jsx
@@ -54,47 +54,49 @@ const SellerItems = ({isSameUser}) => {
     const [items, setItems] = useState(itemsStaticData);
     const [selected, setSelected] = useState(sortBy[0])
     const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+    const [selectedSubCategory, setSelectedSubCategory] = useState(selectedCategory.subCategory[0]);
 
     return (
         <>
-            <div className={'flex flex-row justify-between items-center'}>
-                <div className={'text-sm sm:text-lg font-medium pl-8'}>{items.length} items</div>
+            <div className={'flex flex-col lg:flex-row justify-between items-center'}>
+                <div className={'text-sm sm:text-lg font-medium lg:pl-8'}>{items.length} items</div>
                 {isSameUser === true ? <></> : <>
-                    <div className={'flex flex-row gap-2 items-center'}>
-                        <div className={'w-[10rem]'}>
-                            <Listbox value={selectedCategory} onChange={setSelectedCategory}>
-                                <div className="relative mt-1">
-                                    <Listbox.Button
-                                        className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 text-xs">
-                                        <span className="block truncate">{selectedCategory.name}</span>
-                                        <span
-                                            className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <div className={'flex flex-col lg:flex-row gap-1 lg:gap-2 items-center'}>
+                        <div className={'flex flex-row items-center gap-1 lg:gap-2'}>
+                            <div className={'w-[9rem] lg:w-[12rem]'}>
+                                <Listbox value={selectedCategory} onChange={setSelectedCategory}>
+                                    <div className="relative mt-1">
+                                        <Listbox.Button
+                                            className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 text-xs">
+                                            <span className="block truncate">{selectedCategory.name}</span>
+                                            <span
+                                                className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <SelectorIcon
                   className="h-5 w-5 text-gray-400"
                   aria-hidden="true"
               />
             </span>
-                                    </Listbox.Button>
-                                    <Transition
-                                        as={Fragment}
-                                        leave="transition ease-in duration-100"
-                                        leaveFrom="opacity-100"
-                                        leaveTo="opacity-0"
-                                    >
-                                        <Listbox.Options
-                                            className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-xs shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                            {categories.map((category) => (
-                                                <Listbox.Option
-                                                    key={category.id}
-                                                    className={({active}) =>
-                                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                                            active ? 'bg-red-100 text-red-900' : 'text-gray-900'
-                                                        }`
-                                                    }
-                                                    value={category}
-                                                >
-                                                    {({selected}) => (
-                                                        <>
+                                        </Listbox.Button>
+                                        <Transition
+                                            as={Fragment}
+                                            leave="transition ease-in duration-100"
+                                            leaveFrom="opacity-100"
+                                            leaveTo="opacity-0"
+                                        >
+                                            <Listbox.Options
+                                                className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-xs shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                                {categories.map((category) => (
+                                                    <Listbox.Option
+                                                        key={category.id}
+                                                        className={({active}) =>
+                                                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                                                active ? 'bg-red-100 text-red-900' : 'text-gray-900'
+                                                            }`
+                                                        }
+                                                        value={category}
+                                                    >
+                                                        {({selected}) => (
+                                                            <>
                                                 <span
                                                     className={`block truncate ${
                                                         selected ? 'font-medium' : 'font-normal'
@@ -102,22 +104,79 @@ const SellerItems = ({isSameUser}) => {
                                                 >
                                                 {category.name}
                                                 </span>
-                                                            {selected ? (
-                                                                <span
-                                                                    className="absolute inset-y-0 left-0 flex items-center pl-3 text-susty">
+                                                                {selected ? (
+                                                                    <span
+                                                                        className="absolute inset-y-0 left-0 flex items-center pl-3 text-susty">
                           <CheckIcon className="h-5 w-5" aria-hidden="true"/>
                         </span>
-                                                            ) : null}
-                                                        </>
-                                                    )}
-                                                </Listbox.Option>
-                                            ))}
-                                        </Listbox.Options>
-                                    </Transition>
-                                </div>
-                            </Listbox>
+                                                                ) : null}
+                                                            </>
+                                                        )}
+                                                    </Listbox.Option>
+                                                ))}
+                                            </Listbox.Options>
+                                        </Transition>
+                                    </div>
+                                </Listbox>
+                            </div>
+                            <div className={'w-[10rem] lg:w-[12rem]'}>
+                                <Listbox value={selectedSubCategory} onChange={setSelectedSubCategory}>
+                                    <div className="relative mt-1">
+                                        <Listbox.Button
+                                            className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 text-xs">
+                                            <span className="block truncate">{selectedSubCategory.name}</span>
+                                            <span
+                                                className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <SelectorIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+              />
+            </span>
+                                        </Listbox.Button>
+                                        <Transition
+                                            as={Fragment}
+                                            leave="transition ease-in duration-100"
+                                            leaveFrom="opacity-100"
+                                            leaveTo="opacity-0"
+                                        >
+                                            <Listbox.Options
+                                                className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-xs shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                                {selectedCategory.subCategory.map((category) => (
+                                                    <Listbox.Option
+                                                        key={category.id}
+                                                        className={({active}) =>
+                                                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                                                active ? 'bg-red-100 text-red-900' : 'text-gray-900'
+                                                            }`
+                                                        }
+                                                        value={category}
+                                                    >
+                                                        {({selected}) => (
+                                                            <>
+                                                <span
+                                                    className={`block truncate ${
+                                                        selected ? 'font-medium' : 'font-normal'
+                                                    }`}
+                                                >
+                                                {category.name}
+                                                </span>
+                                                                {selected ? (
+                                                                    <span
+                                                                        className="absolute inset-y-0 left-0 flex items-center pl-3 text-susty">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true"/>
+                        </span>
+                                                                ) : null}
+                                                            </>
+                                                        )}
+                                                    </Listbox.Option>
+                                                ))}
+                                            </Listbox.Options>
+                                        </Transition>
+                                    </div>
+                                </Listbox>
+                            </div>
                         </div>
-                        <div className={'w-[10rem]'}>
+                        <div className={'w-[10rem] lg:w-[12rem]'}>
                             <Listbox value={selected} onChange={setSelected}>
                                 <div className="relative mt-1">
                                     <Listbox.Button
@@ -138,7 +197,7 @@ const SellerItems = ({isSameUser}) => {
                                         leaveTo="opacity-0"
                                     >
                                         <Listbox.Options
-                                            className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-xs shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                            className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-xs shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                                             {sortBy.map((sortedBy) => (
                                                 <Listbox.Option
                                                     key={sortedBy.id}
