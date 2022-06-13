eb0fb4774f3c6eb01950388695805b4babeb3455
Yasas Lowe <64888128+Flashminat0@users.noreply.github.com>
2022-06-13 20:02:10 +0530

    Revert "UserProfile remake as seller profile"

--- pages/final/profile.js
+++ pages/final/profile.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ProfilePageWrapper from "../../components/layouts/ProfilePageWrapper";
 import ProfileBio from "../../components/member/ProfileBio";
 import FooterSection from "../../components/footer/FooterSection";
-import BasicPageWrapper from "../../components/layouts/BasicPageWrapper";
 
 const Profile = () => {
     return (
-        <BasicPageWrapper>
-            <ProfilePageWrapper>
-                <ProfileBio/>
-                <FooterSection/>
-            </ProfilePageWrapper>
-        </BasicPageWrapper>
+        <ProfilePageWrapper>
+            <ProfileBio/>
+            <FooterSection/>
+        </ProfilePageWrapper>
     );
 };
 
