1c50c82e5bf7ca24c6056162551c0f46869eea43
Osini Kithma <66475844+swee76@users.noreply.github.com>
2022-07-13 00:17:28 +0530

    changes in select privacy settings

--- components/settings/privacysettings/PrivacySettings.jsx
+++ components/settings/privacysettings/PrivacySettings.jsx
@@ -13,109 +13,105 @@ const PrivacySettings = () => {
   return (
         <div className={""}>
             <>
-                <div className={"w-full bg-gray-50"}>
+                <div className={"w-full bg-white"}>
                     <div className='p-3'>
-                        <p>Privacy settings</p>
+                        <p className='text-sm text-gray-500'>Privacy settings</p>
                     </div>
-                    <div className={"grid grid-cols-5 gap-5 p-5"}>
+                    <div className={"grid grid-cols-5 gap-5 p-5 border-b border-gray-200"}>
                         <div className={"col-span-4"}>
-                            <p className={"font-semibold"}>Allow Susty to use my pictures for marketing purposes</p>
+                            <p className={"font-medium text-gray-800"}>Allow Susty to use my pictures for marketing purposes</p>
                         </div>
                         <div className={"col-span-1 ml-auto"}>
                             <Switch
                                 checked={allowPictures}
                                 onChange={setAllowPictures}
                                 className={`${allowPictures ? 'bg-susty' : 'bg-gray-300'}
-                                relative inline-flex h-[30px] w-[60px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+                                relative inline-flex h-[24px] w-[48px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
                             >
                                 <span className="sr-only">Use setting</span>
                                 <span
                                     aria-hidden="true"
-                                    className={`${allowPictures ? 'translate-x-[1.855rem]' : 'translate-x-0'}
-                                pointer-events-none inline-block h-[26px] w-[26px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                                    className={`${allowPictures ? 'translate-x-[1.53rem]' : 'translate-x-0'}
+                            pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
                                 />
                             </Switch>
                         </div>
                     </div>
-                    <hr className={`my-1`}/>
                 </div>
-                <div className={"w-full bg-gray-50"}>
-                    <div className={"grid grid-cols-5 gap-5 p-5"}>
+                <div className={"w-full bg-white"}>
+                    <div className={"grid grid-cols-5 gap-5 p-5 border-b border-gray-200"}>
                         <div className={"col-span-4"}>
-                            <p className={"font-semibold"}>Notify owners when I favorite their items</p>
+                            <p className={"font-medium text-gray-800"}>Notify owners when I favorite their items</p>
                         </div>
                         <div className={"col-span-1 ml-auto"}>
                             <Switch
                                 checked={notifyOwners}
                                 onChange={setNotifyOwners}
                                 className={`${notifyOwners ? 'bg-susty' : 'bg-gray-300'}
-                                relative inline-flex h-[30px] w-[60px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+                                relative inline-flex h-[24px] w-[48px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
                             >
                                 <span className="sr-only">Use setting</span>
                                 <span
                                     aria-hidden="true"
-                                    className={`${notifyOwners ? 'translate-x-[1.855rem]' : 'translate-x-0'}
-                                pointer-events-none inline-block h-[26px] w-[26px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                                    className={`${notifyOwners ? 'translate-x-[1.53rem]' : 'translate-x-0'}
+                            pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
                                 />
                             </Switch>
                         </div>
                     </div>
-                    <hr className={`my-1`}/>
                 </div>
-                <div className={"w-full bg-gray-50"}>
-                    <div className={"grid grid-cols-5 gap-5 p-5"}>
+                <div className={"w-full bg-white"}>
+                    <div className={"grid grid-cols-5 gap-5 p-5 border-b border-gray-200"}>
                         <div className={"col-span-4"}>
-                            <p className={"font-semibold"}>Allow third-party tracking</p>
+                            <p className={"font-medium text-gray-800"}>Allow third-party tracking</p>
                         </div>
                         <div className={"col-span-1 ml-auto"}>
                             <Switch
                                 checked={allowThirdPartyTracking}
                                 onChange={setAllowThirdPartyTracking}
                                 className={`${allowThirdPartyTracking ? 'bg-susty' : 'bg-gray-300'}
-                                relative inline-flex h-[30px] w-[60px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+                                relative inline-flex h-[24px] w-[48px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
                             >
                                 <span className="sr-only">Use setting</span>
                                 <span
                                     aria-hidden="true"
-                                    className={`${allowThirdPartyTracking ? 'translate-x-[1.855rem]' : 'translate-x-0'}
-                                pointer-events-none inline-block h-[26px] w-[26px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                                    className={`${allowThirdPartyTracking ? 'translate-x-[1.53rem]' : 'translate-x-0'}
+                            pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
                                 />
                             </Switch>
                         </div>
                     </div>
-                    <hr className={`my-1`}/>
                 </div>
-                <div className={"w-full bg-gray-50"}>
-                    <div className={"grid grid-cols-5 gap-5 p-5"}>
+                <div className={"w-full bg-white"}>
+                    <div className={"grid grid-cols-5 gap-2 p-5 border-b border-gray-200"}>
                         <div className={"col-span-4"}>
-                            <p className={"font-semibold"}>Allow Susty to personalize my feed and search results by evaluating <br></br> my preferences, settings, previous purchases  and usage of  <br></br>Susty website and app</p>
+                            <p className={"font-medium text-gray-800"}>Allow Susty to personalize my feed and search results by evaluating my preferences, settings, previous purchases  and usage of Susty website and app</p>
                         </div>
                         <div className={"col-span-1 ml-auto"}>
                             <Switch
                                 checked={allowToPersonalize}
                                 onChange={setAllowToPersonalize}
                                 className={`${allowToPersonalize ? 'bg-susty' : 'bg-gray-300'}
-                                relative inline-flex h-[30px] w-[60px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+                                relative inline-flex h-[24px] w-[48px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
                             >
                                 <span className="sr-only">Use setting</span>
                                 <span
                                     aria-hidden="true"
-                                    className={`${allowToPersonalize ? 'translate-x-[1.855rem]' : 'translate-x-0'}
-                                pointer-events-none inline-block h-[26px] w-[26px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                                    className={`${allowToPersonalize ? 'translate-x-[1.53rem]' : 'translate-x-0'}
+                            pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
                                 />
                             </Switch>
                         </div>
                     </div>
-                    <hr className={`my-1`}/>
                 </div>
 
-                <div className={"w-full bg-gray-50 p-5 hover:bg-gray-100 transition ease-in-out delay-50 cursor-pointer"}
+                <div className={"w-full bg-white p-5 hover:bg-gray-50 transition ease-in-out delay-50 cursor-pointer"}
                     onClick = {() => setEnableDownloadData(true)}
                 >
-                    <li className="group flex justify-between">
+                    <li className="group flex justify-between items-center">
                         <div>
-                            <p className={"font-semibold"}>Download account data</p>
-                            <p className={"font-thin"}>Request account data</p>
+                            <p className={"font-medium text-gray-800"}>Download account data</p>
+                            <p className={"font-thin text-gray-500 text-sm"}>Request a copy of your susty account data</p>
                         </div>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#7d7d7d" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
